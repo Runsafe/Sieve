b6e76524f9eff0d5088dfd0e72fbcde1c99e9e30
Morten Nilsen <morten@runsafe.no>
2014-04-29 23:35:26 +0200

    Initialized new plugin Sieve from template

---
+++ src/no/runsafe/sieve/Sieve.java
@@ -0,0 +1,26 @@
+package no.runsafe.sieve;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class Sieve extends RunsafePlugin
+//public class Sieve extends RunsafeConfigurablePlugin
+{
+//	public static IDebug Debugger = null;
+
+	@Override
+	protected void pluginSetup()
+	{
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
+	}
+}
